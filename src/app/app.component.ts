55cfdc8705c3cfd6019cb11df05af85d55d0da25
SilviaGonz <151831773+SilviaGonz@users.noreply.github.com>
2025-03-18 19:17:41 +0000

    create the header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ButonComponent } from './buton/buton.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, ButonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'HireMe';
 }
