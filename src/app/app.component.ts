dc460d02f2eedfa9dc414eb59bfda04949b018c9
SilviaGonz <151831773+SilviaGonz@users.noreply.github.com>
2025-03-20 10:06:13 +0000

    import the headerWithLogin component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { ButonComponent } from './buton/buton.component';
+import {HeaderWithLoginComponent} from './header-with-login/header-with-login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ButonComponent],
+  imports: [RouterOutlet, HeaderComponent, ButonComponent, HeaderWithLoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
