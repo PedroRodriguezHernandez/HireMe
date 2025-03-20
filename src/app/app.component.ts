1be6983757a853015e580b724771f04f47cfde21
Pedro Manuel <pedrom.rodriguezhernandez@gmail.com>
2025-03-20 17:54:04 +0000

    footer-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { ButonComponent } from './buton/buton.component';
 import {HeaderWithLoginComponent} from './header-with-login/header-with-login.component';
+import {FooterComponent} from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ButonComponent, HeaderWithLoginComponent],
+  imports: [RouterOutlet, HeaderComponent, ButonComponent, HeaderWithLoginComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
