c3f383129351119203aed65607ddd196bda87b1e
Pablo <zerytv75@gmail.com>
2025-03-24 20:36:04 +0000

    Profile maomeno

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,13 @@ import {HeaderWithLoginComponent} from './header-with-login/header-with-login.co
 import {LateralOrangeComponent} from './lateral-orange/lateral-orange.component';
 import {LoginPageComponent} from './login-page/login-page.component';
 import {TextInputComponent} from './text-input/text-input.component';
+import {ProfileComponent} from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ButonComponent, HeaderWithLoginComponent, LateralOrangeComponent, LoginPageComponent, TextInputComponent],
+  imports: [RouterOutlet, HeaderComponent, ButonComponent, HeaderWithLoginComponent, LateralOrangeComponent,
+    LoginPageComponent, TextInputComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
