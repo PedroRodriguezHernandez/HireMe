71eb2fad433d17b146a6d02027f28e89445b71b8
SilviaGonz <151831773+SilviaGonz@users.noreply.github.com>
2025-03-24 18:53:04 +0000

    replace header components with login page component and update imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { ButonComponent } from './buton/buton.component';
 import {HeaderWithLoginComponent} from './header-with-login/header-with-login.component';
+import {LateralOrangeComponent} from './lateral-orange/lateral-orange.component';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {TextInputComponent} from './text-input/text-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ButonComponent, HeaderWithLoginComponent],
+  imports: [RouterOutlet, HeaderComponent, ButonComponent, HeaderWithLoginComponent, LateralOrangeComponent, LoginPageComponent, TextInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
