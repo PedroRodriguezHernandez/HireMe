45f4964bf8e1e1dfa19b6d8736086dfbba0c7e78
jorgealamoo <jorgealamosan@gmail.com>
2025-04-08 17:07:48 +0100

    feat: implement routing for login and my offers pages

---
+++ src/app/features/my-offers/my-offers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-offers',
+  imports: [],
+  templateUrl: './my-offers.component.html',
+  styleUrl: './my-offers.component.css'
+})
+export class MyOffersComponent {
+
+}
