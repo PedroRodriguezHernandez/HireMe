caef191eb5b86569c6485abf4d1120a1cd4d770c
jorgealamoo <jorgealamosan@gmail.com>
2025-04-08 16:38:06 +0100

    refactor login page with reactive forms and update button component

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -2,18 +2,53 @@ import { Component } from '@angular/core';
 import {LateralOrangeComponent} from '../lateral-orange/lateral-orange.component';
 import {TextInputComponent} from '../text-input/text-input.component';
 import {ButonComponent} from '../buton/buton.component';
+import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {SupabaseService} from '../supabase.service';
+import {NgIf} from '@angular/common';
+import {showAlert} from '../../services/utils';
 
 @Component({
   selector: 'app-login-page',
   imports: [
     LateralOrangeComponent,
     TextInputComponent,
-    ButonComponent
+    ButonComponent,
+    ReactiveFormsModule,
+    NgIf
   ],
   templateUrl: './login-page.component.html',
   standalone: true,
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+  errorMessage: string = '';
 
+  constructor(private supabaseService: SupabaseService) {
+
+  }
+
+  login() {
+    console.log(this.loginForm.value);
+    if (this.loginForm.valid) {
+      const credentials = {
+        email: this.loginForm.value.email ?? "",
+        password: this.loginForm.value.password ?? ""
+      };
+
+      this.supabaseService.signIn(credentials)
+        .then(response => {
+          console.log('Login successful:', response);
+        })
+        .catch(async error => {
+          showAlert("Email or password is incorrect", "Please check your email and password and try again.");
+        });
+    } else {
+      console.log("Form is invalid");
+      showAlert("Invalid form", "Please check your email and password.");
+    }
+  }
 }
--- src/app/text-input/text-input.component.ts
+++ src/app/text-input/text-input.component.ts
@@ -1,13 +1,17 @@
 import {Component, Input} from '@angular/core';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-text-input',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './text-input.component.html',
   standalone: true,
   styleUrl: './text-input.component.css'
 })
 export class TextInputComponent {
+  @Input() control!: FormControl;
   @Input() placeholder: string = '';
   @Input() type: string = 'text';
 }
