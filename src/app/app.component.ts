19a52fc071fbca662da7fc20e81f15fddc765c7a
SilviaGonz <151831773+SilviaGonz@users.noreply.github.com>
2025-04-21 23:22:54 +0100

    refactor: update profile layout and integrate header with login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { ButonComponent } from './buton/buton.component';
 import {HeaderWithLoginComponent} from './header-with-login/header-with-login.component';
-import {CreateComponent} from './create/create.component';
-import {LateralOrangeComponent} from './lateral-orange/lateral-orange.component';
 import {LoginPageComponent} from './login-page/login-page.component';
 import {TextInputComponent} from './text-input/text-input.component';
 import {FooterComponent} from './footer/footer.component';
@@ -15,7 +13,6 @@ import {ProfileComponent} from './profile/profile.component';
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, ButonComponent, HeaderWithLoginComponent,
     LoginPageComponent, TextInputComponent, ProfileComponent, FooterComponent],
-  imports: [RouterOutlet, HeaderComponent, ButonComponent, HeaderWithLoginComponent, FooterComponent, CreateComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import {ButonComponent} from '../buton/buton.component';
 import {SupabaseService} from '../../services/supabase.service';
 import {Router} from '@angular/router';
+import {HeaderWithLoginComponent} from '../header-with-login/header-with-login.component';
 
 @Component({
   selector: 'app-profile',
   imports: [
-    ButonComponent
+    ButonComponent,
+    HeaderWithLoginComponent
   ],
   templateUrl: './profile.component.html',
   standalone: true,
