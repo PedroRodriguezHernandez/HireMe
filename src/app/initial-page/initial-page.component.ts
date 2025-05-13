62979aa595c42a1acdc6ed72bbc69ba642a9cfbd
SilviaGonz <151831773+SilviaGonz@users.noreply.github.com>
2025-05-13 17:30:42 +0100

    feat: implement conditional header rendering based on user login status

--- src/app/initial-page/initial-page.component.ts
+++ src/app/initial-page/initial-page.component.ts
@@ -1,24 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {FooterComponent} from '../footer/footer.component';
 import {HeaderInitialPageComponent} from '../header-initial-page/header-initial-page.component';
 import {Router} from '@angular/router';
+import {AuthInterface} from '../core/interface/auth-interface';
+import {SupabaseAuthClient} from '@supabase/supabase-js/dist/main/lib/SupabaseAuthClient';
+import {NgIf} from '@angular/common';
+import {HeaderWithLoginComponent} from '../header-with-login/header-with-login.component';
+import {AuthSupabaseService} from '../core/services/auth-supabase.service';
+import * as console from 'console';
 
 @Component({
   selector: 'app-initial-page',
   imports: [
     FooterComponent,
-    HeaderInitialPageComponent
+    HeaderInitialPageComponent,
+    NgIf,
+    HeaderWithLoginComponent
   ],
   templateUrl: './initial-page.component.html',
   standalone: true,
   styleUrl: './initial-page.component.css'
 })
-export class InitialPageComponent {
+export class InitialPageComponent implements OnInit{
+  async ngOnInit() {
+    await this.auth.getCurrentUser().subscribe({
+      next: authUser => {
+        this.login = !!authUser;
+      },
+      error: err => {
+        this.login = false;
+      }
+    });
+  }
+
 
   protected readonly alert = alert;
-
-  constructor(private router: Router) {
-  }
+  protected login: boolean = false;
+  constructor(
+    private router: Router,
+    @Inject(AuthSupabaseService) protected auth: AuthInterface
+  ) {}
 
   onSearch(value: string) {
     const trimmed = value.trim();
