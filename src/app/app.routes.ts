363d3a473324f6fa953b5a3b4878f154a8aa1a72
Pedro Manuel <pedrom.rodriguezhernandez@gmail.com>
2025-05-13 16:43:50 +0100

    Router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,31 +1,47 @@
-import { Routes } from '@angular/router';
+import {authGuard} from './core/guards/auth.guard';
+import {Routes} from '@angular/router';
+
 export const routes: Routes = [
   {
-    path: "my-offers", loadComponent: () => import("./features/my-offers/my-offers.component")
+    path: "my-offers",
+    canActivate: [authGuard],
+    loadComponent: () => import("./features/my-offers/my-offers.component")
       .then(m => m.MyOffersComponent)
   },
   {
-    path: "my-profile", loadComponent: () => import("./profile/profile.component")
+    path: "my-profile",
+    canActivate: [authGuard],
+    loadComponent: () => import("./profile/profile.component")
       .then(m => m.ProfileComponent)
   },
   {
-    path: "login", loadComponent: () => import("./login-page/login-page.component")
-      .then(m => m.LoginPageComponent)
-  },
-  {
-    path: "create", loadComponent: () => import("./create/create.component")
+    path: "create",
+    canActivate: [authGuard],
+    loadComponent: () => import("./create/create.component")
       .then(m => m.CreateComponent)
   },
   {
-    path: "home", loadComponent: () => import("./features/home-page/home-page.component")
+    path: "home",
+    canActivate: [authGuard],
+    loadComponent: () => import("./features/home-page/home-page.component")
       .then(m => m.HomePageComponent)
   },
   {
-    path: "register", loadComponent: () => import("./register-page/register-page.component")
+    path: "login",
+    canActivate: [authGuard],
+    loadComponent: () => import("./login-page/login-page.component")
+      .then(m => m.LoginPageComponent)
+  },
+  {
+    path: "register",
+    canActivate: [authGuard],
+    loadComponent: () => import("./register-page/register-page.component")
       .then(m => m.RegisterPageComponent)
   },
   {
-    path: "", loadComponent: () => import("./initial-page/initial-page.component")
+    path: "",
+    canActivate: [authGuard],
+    loadComponent: () => import("./initial-page/initial-page.component")
       .then(m => m.InitialPageComponent)
   },
 ];
